8d9221dcd8efb02347e6d2874a6a7bcac56b12c8
Ed1l3udo <edileudommfilho@gmail.com>
2025-06-11 14:28:24 -0300

    revertendo mudanças para o ultimo commit

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -4,27 +4,41 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 5f;
+    private Animator animator;
+    private bool isMoving = false;
+    private bool isRunning = false;
     Vector2 moveInput;
 
-    public bool isMoving { get; private set; }
+    public bool isMoving { 
+        get
+        {
+            return isMoving;
+        }
+        private set
+        {
+            isMoving = value;
+        }
+        animator.SetBool("isMoving", value);
+    }
+
+    public bool isRunning {
+        get
+        {
+            return isRunning;
+        }
+        private set
+        {
+            isRunning = value;
+        }
+        animator.SetBool("isRunning", value);
+    }
 
     Rigidbody2D rb;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -37,5 +51,10 @@ public class PlayerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
 
         isMoving = (moveInput != Vector2.zero);
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        isRunning = context.ReadValue<bool>;
     }
 }
